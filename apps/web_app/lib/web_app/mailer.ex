377236cdc58508d855800884ca2755df2eecae70
Cyber Oliveira <lccro@msn.com>
2022-08-02 21:06:02 -0300

    adds phoenix

---
+++ apps/web_app/lib/web_app/mailer.ex
@@ -0,0 +1,3 @@
+defmodule WebApp.Mailer do
+  use Swoosh.Mailer, otp_app: :web_app
+end
--- config/config.exs
+++ config/config.exs
@@ -1,18 +1,49 @@
-# This file is responsible for configuring your umbrella
-# and **all applications** and their dependencies with the
-# help of the Config module.
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Config module.
 #
-# Note that all applications in your umbrella share the
-# same configuration and dependencies, which is why they
-# all use the same configuration file. If you want different
-# configurations or dependencies per app, it is best to
-# move said applications out of the umbrella.
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+
+# General application configuration
 import Config
 
-# Sample configuration:
+# Configures the endpoint
+config :web_app, WebAppWeb.Endpoint,
+  url: [host: "localhost"],
+  render_errors: [view: WebAppWeb.ErrorView, accepts: ~w(html json), layout: false],
+  pubsub_server: WebApp.PubSub,
+  live_view: [signing_salt: "wWYqfi5f"]
+
+# Configures the mailer
 #
-#     config :logger, :console,
-#       level: :info,
-#       format: "$date $time [$level] $metadata$message\n",
-#       metadata: [:user_id]
+# By default it uses the "Local" adapter which stores the emails
+# locally. You can see the emails in your browser, at "/dev/mailbox".
 #
+# For production it's recommended to configure a different adapter
+# at the `config/runtime.exs`.
+config :web_app, WebApp.Mailer, adapter: Swoosh.Adapters.Local
+
+# Swoosh API client is needed for adapters other than SMTP.
+config :swoosh, :api_client, false
+
+# Configure esbuild (the version is required)
+config :esbuild,
+  version: "0.14.29",
+  default: [
+    args:
+      ~w(js/app.js --bundle --target=es2017 --outdir=../priv/static/assets --external:/fonts/* --external:/images/*),
+    cd: Path.expand("../apps/web_app/assets", __DIR__),
+    env: %{"NODE_PATH" => Path.expand("../deps", __DIR__)}
+  ]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Use Jason for JSON parsing in Phoenix
+config :phoenix, :json_library, Jason
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{config_env()}.exs"
