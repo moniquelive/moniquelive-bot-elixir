77a798b21d76fb116886f9df74ff379cb3d561e1
Cyber Oliveira <lccro@msn.com>
2022-08-28 16:21:35 -0300

    Joins the Supervision tree and changes the strategy to one_for_one

--- apps/chatbot/lib/chatbot/application.ex
+++ apps/chatbot/lib/chatbot/application.ex
@@ -11,31 +11,18 @@ defmodule Chatbot.Application do
     State
   }
 
-  @impl true
+  @impl Application
   def start(_type, _args) do
-    start_chatbot_supervisor()
-    start_spotify_supervisor()
-  end
-
-  defp start_chatbot_supervisor() do
     [bot_config] = Application.fetch_env!(:chatbot, :bots)
 
     children = [
       State,
       {Config, [dirs: [Path.expand("../..", __DIR__)]]},
-      {TMI.Supervisor, bot_config}
-    ]
-
-    opts = [strategy: :one_for_all, name: Chatbot.Supervisor]
-    Supervisor.start_link(children, opts)
-  end
-
-  defp start_spotify_supervisor() do
-    children = [
+      {TMI.Supervisor, bot_config},
       {SpotifyMonitor, Application.fetch_env!(:spotify_ex, :refresh_token)}
     ]
 
-    opts = [strategy: :one_for_one, name: Chatbot.SpotifySupervisor]
+    opts = [strategy: :one_for_one, name: Chatbot.Supervisor]
     Supervisor.start_link(children, opts)
   end
 end
