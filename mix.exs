8781932ca7e860a8acbda28ba758aa96ae5383cb
Cyber Oliveira <lccro@msn.com>
2022-09-16 23:58:55 -0300

    Adds release config in mix.exs

--- mix.exs
+++ mix.exs
@@ -8,7 +8,12 @@ defmodule TwitchBot.MixProject do
       apps_path: "apps",
       version: "0.1.0",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      releases: [
+        default: [
+          applications: [web_app: :permanent]
+        ]
+      ]
     ]
   end
 
