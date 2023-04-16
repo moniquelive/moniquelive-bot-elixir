5a8087317c98f83522e80ba748456a9f44bc4704
Cyber Oliveira <lccro@msn.com>
2023-04-16 02:13:45 -0300

    adds user:manage:chat_color scope

--- README.md
+++ README.md
@@ -2,7 +2,6 @@
 
 ## For now, to auth
 
-* [click here](http://localhost:8090/oauth?scopes[]=channel:read:stream_key&scopes[]=channel:manage:broadcast)
+- [click here](http://localhost:8090/oauth?scopes[]=channel:read:stream_key&scopes[]=channel:manage:broadcast&scopes[]=user:manage:chat_color)
 
 **TODO: Add description**
-
--- apps/chatbot/config/config.exs
+++ apps/chatbot/config/config.exs
@@ -15,7 +15,7 @@ config :elixir, :time_zone_database, Tzdata.TimeZoneDatabase
 
 config :scrapped_twitch_api,
   default_adapter: "https://dev.twitch.tv/docs/api/reference",
-  access_token_scopes: ~w(user:read:email channel:manage:broadcast),
+  access_token_scopes: ~w(user:read:email channel:manage:broadcast user:manage:chat_color),
   callback_protocol: :http,
   callback_uri: "localhost",
   callback_port: 8090
