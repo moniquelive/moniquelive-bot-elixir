ab87e377c472bd5872b4ca04c0165fbae9d96ec2
Monique Oliveira <lccro@msn.com>
2025-07-05 19:01:21 -0300

    fix: elm audio play

--- apps/web_app/assets/elm/src/index.js
+++ apps/web_app/assets/elm/src/index.js
@@ -7,11 +7,10 @@ if (rootNode !== null) {
   const app = Elm.Main.init({ node: rootNode });
   ElmPhoenixWebSocket.init(app.ports, Socket, Presence);
   app.ports.playMP3.subscribe(function(payload) {
-    const audio = new Audio({
-      src: "data:audio/mp3;base64," + payload,
-      volume: 1.0,
-    });
     // navigator.mediaDevices.getUserMedia({ audio: true, video: false }).then(() => audio.play());
+    const audio = new Audio();
+    audio.src = "data:audio/mp3;base64," + payload;
+    audio.volume = 1.0;
     audio.play();
   });
 }
